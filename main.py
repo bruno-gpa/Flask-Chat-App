5b8010ac4a2d5197e1d15a84c3c9863f2fa85226
Bruno Azevedo <70606968+bruno-gpa@users.noreply.github.com>
2021-01-06 22:27:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request
 from datetime import datetime
 from flask_socketio import SocketIO
 from components.database import DataBase
+from components.utilities import *
 
 # app and socket init
 app = Flask(__name__)
@@ -10,7 +11,7 @@ socketio = SocketIO(app)
 
 # database and globals init
 db = DataBase()
-users = []
+rooms = {}
 
 
 # renders login page
@@ -19,23 +20,30 @@ def home():
 
 	if request.method == 'POST':
 		if request.form['user'] != '':
-			user = request.form['user']
-			db.create_table(user)
-			return redirect(url_for("chat", user=user))
+			user = request.form['user'].capitalize()
+			code = str(request.form['room'])
+
+			if not check_valid(code):
+				code = generate_code(rooms)
+
+			if check_username(user) and check_online(rooms, code, user):
+				db.create_table(user)
+				code = f"{code}-{user}"
+				return redirect(url_for("chat", code=code))
 
 	return render_template("home.html")
 
 
 # renders chat page
-@app.route("/chat/<user>", methods=["GET", "POST"])
-def chat(user):
-	return render_template("chat.html", user=user)
+@app.route("/chat/<code>", methods=["GET", "POST"])
+def chat(code):
+	return render_template("chat.html", code=code)
 
 
 # renders chat history page
-@app.route("/chat/<user>/history", methods=["GET", "POST"])
-def history(user):
-	raw = db.get(user)
+@app.route("/chat/<code>/history", methods=["GET", "POST"])
+def history(code):
+	raw = db.get(code[5:])
 	messages = [(x[1], x[2]) for x in raw]
 
 	if messages:
@@ -45,49 +53,53 @@ def history(user):
 	else:
 		age, count = "N/A", "N/A"
 
-	return render_template("history.html", user=user, messages=messages, age=age, count=count)
+	return render_template("history.html", code=code, messages=messages, age=age, count=count)
 
 
 # deletes account and information
 @app.route("/delete-account/<user>")
 def delete(user):
 	db.drop_table(user)
-
 	return redirect(url_for("home"))
 
 
 # method for socket broadcast
 @socketio.on("message")
 def handle_my_custom_event(json):
-	global users
+	global rooms
+	user, code = json["user"], json["room"]
+	print(code)
 	dnow = datetime.now()
 
-	if json["user"] not in users:
-		users.append(json["user"])
+	if code not in rooms:
+		rooms[code] = []
+	if user not in rooms[code]:
+		rooms[code].append(user)
 		
-		print(f"\n[Current connections] {len(users)}")
-		print(f"[Current users] {users}\n")
+		print(f"\n[Current connections] {len(rooms[code])}")
+		print(f"[Current users] {rooms[code]}\n")
 
 	print(f"\n[Message received] {json}\n")
 
 	if "data" in json:
-		db.append(json["user"], json["data"], dnow.strftime("%d/%m/%Y %H:%M"))
+		db.append(user, json["data"], dnow.strftime("%d/%m/%Y %H:%M"))
 		socketio.emit("relay", json)
 	else:
-		socketio.emit("online now", str(len(users)))
+		socketio.emit("online now", str(len(rooms[code])))
 
 
 # method for socket disconnection
 @socketio.on("disconnection")
 def handle_disconnection(json):
-	global users
-	users.remove(json["user"])
+	global rooms
+	user, code = json["user"], json["room"]
+	rooms[code].remove(user)
 
 	print("\n[User disconnected]")
-	print(f"[Current connections] {len(users)}")
-	print(f"[Current users] {users}\n")
+	print(f"[Current connections] {len(rooms[code])}")
+	print(f"[Current users] {rooms[code]}\n")
 
-	socketio.emit("online now", str(len(users)))
+	socketio.emit("online now", str(len(rooms[code])))
 
 
 # SocketIO app run
